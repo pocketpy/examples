6238ba953783160accac47d37f661d91f450cd12
blueloveTH <blueloveTH@foxmail.com>
2024-05-01 20:00:09 +0800

    Update main.cpp

--- examples/05 Bind Simple Struct/main.cpp
+++ examples/05 Bind Simple Struct/main.cpp
@@ -9,62 +9,43 @@ Str read_stdin(){
 }
 
 struct Point{
+    PY_CLASS(Point, builtins, Point)
+
     int x;
     int y;
+
+    void init(int x, int y){
+        this->x = x;
+        this->y = y;
+    }
+
+    Point* _() {
+        return this;
+    }
 
     float distance(){
         return std::sqrt(x*x + y*y);
     }
-};
 
-struct wrapped__Point{
-    // special macro for wrapper class
-    PY_CLASS(wrapped__Point, builtins, Point)
-    //       ^T              ^module   ^name
-
-    // wrapped value
-    Point value;
-
-    // special method _ returns a pointer of the wrapped value
-    Point* _() { return &value; }
-
-    // define default constructors
-    wrapped__Point() = default;
-    wrapped__Point(const wrapped__Point&) = default;
-
-    // define wrapped constructor
-    wrapped__Point(Point value){
-        this->value = value;
+    bool operator==(Point other){
+        return x==other.x && y==other.y;
     }
 
     static void _register(VM* vm, PyObject* mod, PyObject* type){
-        // enable default constructor and struct-like methods
-        // if you don't use this, you must bind a `__new__` method as constructor
-        PY_STRUCT_LIKE(wrapped__Point)
+        PY_STRUCT_LIKE(Point)
 
-        // wrap field x
-        PY_FIELD(wrapped__Point, "x", _, x)
-        // wrap field y
-        PY_FIELD(wrapped__Point, "y", _, y)
+        PY_FIELD(Point, "x", _, x)
+        PY_FIELD(Point, "y", _, y)
 
-        // __init__ method
-        vm->bind(type, "__init__(self, x, y)", [](VM* vm, ArgsView args){
-            wrapped__Point& self = _py_cast<wrapped__Point&>(vm, args[0]);
-            self.value.x = py_cast<int>(vm, args[1]);
-            self.value.y = py_cast<int>(vm, args[2]);
-            return vm->None;
-        });
-
-        vm->bind(type, "distance(self)", [](VM* vm, ArgsView args){
-            wrapped__Point& self = _py_cast<wrapped__Point&>(vm, args[0]);
-            return VAR(self.value.distance());
-        });
+        _bind(vm, type, "__init__(self, x, y)", &Point::init);
+        _bind(vm, type, "distance(self)", &Point::distance);
     }
 };
 
+
 int main(){
     VM* vm = new VM();
-    wrapped__Point::register_class(vm);
+    Point::register_class(vm, vm->builtins);
     vm->exec(read_stdin());
     delete vm;
     return 0;
