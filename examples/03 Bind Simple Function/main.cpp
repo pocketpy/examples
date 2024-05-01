a28bb44314e6ebf7b82373c0893422aa44250d25
blueloveTH <blueloveTH@foxmail.com>
2024-05-01 18:45:38 +0800

    some update

---
+++ examples/03 Bind Simple Function/main.cpp
@@ -0,0 +1,29 @@
+#include "pocketpy.h"
+
+using namespace pkpy;
+
+Str read_stdin(){
+    SStream ss;
+    char ch;
+    while(std::cin.get(ch)) ss << ch;
+    return ss.str();
+}
+
+int main(){
+    // Create a virtual machine
+    VM* vm = new VM();
+
+    // Bind a function named "add" to the main module
+    vm->bind(vm->_main, "add(a: int, b: int)", [](VM* vm, ArgsView args){
+        int a = py_cast<int>(vm, args[0]);
+        int b = py_cast<int>(vm, args[1]);
+        return py_var(vm, a + b);
+    });
+
+    // Run code
+    vm->exec(read_stdin());
+
+    // Dispose the virtual machine
+    delete vm;
+    return 0;
+}
