54fbf4c9fbe8d341438c3acd1f4af567120ee2ca
blueloveTH <blueloveTH@foxmail.com>
2024-05-01 18:04:27 +0800

    Update build.py

--- build.py
+++ build.py
@@ -3,13 +3,17 @@ import json
 
 DATA = []
 
+def read_file(path):
+    with open(path, 'rt') as f:
+        return f.read()
+
 for name in os.listdir('examples'):
     source = os.path.join('examples', name, 'main.cpp')
     input = os.path.join('examples', name, 'input.txt')
-    DATA.append([name], {'source': source, 'input': input})
+    DATA.append([name, {'source': read_file(source), 'input': read_file(input)}])
 
 with open('docs/src/data.js', 'wt') as f:
     f.write('const DATA = ')
-    json.dump(DATA, f)
+    json.dump(DATA, f, indent=4)
     f.write(';\n')
     f.write('export default DATA;\n')
