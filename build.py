8c4e88f64f9936898384d0e5bdfd343c0de5ddb4
blueloveTH <blueloveth@foxmail.com>
2025-03-01 00:15:34 +0800

    update

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ def read_file(path):
         return f.read()
 
 for name in sorted(os.listdir('examples')):
-    source = os.path.join('examples', name, 'main.cpp')
+    source = os.path.join('examples', name, 'main.c')
     input = os.path.join('examples', name, 'input.txt')
     DATA.append([name, {'source': read_file(source), 'input': read_file(input)}])
 
---
+++ examples/01 Hello World/main.c
@@ -0,0 +1,21 @@
+#include "pocketpy.h"
+#include <assert.h>
+
+int main(){
+    // Initialize python environment
+    // Call this function before using any other function from `pocketpy.h`
+    py_initialize();
+
+    // Hello world!
+    bool ok = py_exec("print('Hello world!')", "<string>", EXEC_MODE, NULL);
+    if(!ok){
+        // In case of error, print the exception message
+        py_printexc();
+    }
+
+    // Finalize python environment
+    // Call this function in the end of the program
+    // After this call, you should not use any function from `pocketpy.h`
+    py_finalize();
+    return 0;
+}
