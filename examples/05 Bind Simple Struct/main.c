00ad0ce85f9887a3c6f605d0d34976db7e26d7ec
blueloveTH <blueloveth@foxmail.com>
2025-03-01 15:33:25 +0800

    some fix

--- examples/05 Bind Simple Struct/main.c
+++ examples/05 Bind Simple Struct/main.c
@@ -1,44 +1,91 @@
 #include "pocketpy.h"
-#include <iostream>
+#include <math.h>
 
-using namespace pkpy;
+#define INPUT ""
 
-Str read_stdin(){
-    SStream ss; char ch;
-    while(std::cin.get(ch)) ss << ch;
-    return ss.str();
+typedef struct {
+  float x;
+  float y;
+} Vector2;
+
+static bool Vector2__new__(int argc, py_Ref argv) {
+  py_Type cls = py_totype(argv);
+  py_newobject(py_retval(), cls, 0, sizeof(Vector2));
+  return true;
 }
 
-struct Point{
-    int x;
-    int y;
+static bool Vector2__init__(int argc, py_Ref argv) {
+  PY_CHECK_ARGC(3);
+  Vector2 *self = py_touserdata(py_arg(0));
+  if (!py_castfloat32(py_arg(1), &self->x)) {
+    return false;
+  }
+  if (!py_castfloat32(py_arg(2), &self->y)) {
+    return false;
+  }
+  py_newnone(py_retval());
+  return true;
+}
 
-    void __init__(int x, int y){
-        this->x = x;
-        this->y = y;
-    }
+static bool Vector2_get_x(int argc, py_Ref argv) {
+  PY_CHECK_ARGC(1);
+  Vector2 *self = py_touserdata(py_arg(0));
+  py_newfloat(py_retval(), self->x);
+  return true;
+}
 
-    float distance(){
-        return std::sqrt(x*x + y*y);
-    }
-};
+static bool Vector2_get_y(int argc, py_Ref argv) {
+  PY_CHECK_ARGC(1);
+  Vector2 *self = py_touserdata(py_arg(0));
+  py_newfloat(py_retval(), self->y);
+  return true;
+}
 
+static bool Vector2_set_x(int argc, py_Ref argv) {
+  PY_CHECK_ARGC(2);
+  Vector2 *self = py_touserdata(py_arg(0));
+  if (!py_castfloat32(py_arg(1), &self->x)) {
+    return false;
+  }
+  py_newnone(py_retval());
+  return true;
+}
 
-int main(){
-    VM* vm = new VM();
-    // Create a new module named "test"
-    PyObject* mod = vm->new_module("test");
-    // Register the Point class to the module
-    vm->register_user_class<Point>(mod, "Point",
-    [](VM* vm, PyObject* mod, PyObject* type){
-        vm->bind_field(type, "x", &Point::x);
-        vm->bind_field(type, "y", &Point::y);
+static bool Vector2_set_y(int argc, py_Ref argv) {
+  PY_CHECK_ARGC(2);
+  Vector2 *self = py_touserdata(py_arg(0));
+  if (!py_castfloat32(py_arg(1), &self->y)) {
+    return false;
+  }
+  py_newnone(py_retval());
+  return true;
+}
 
-        vm->bind(type, "__init__(self, x, y)", &Point::__init__);
-        vm->bind(type, "distance(self)", &Point::distance);
-    }, VM::tp_object, true);
-    // Run code
-    vm->exec(read_stdin());
-    delete vm;
-    return 0;
+static bool Vector2_length(int argc, py_Ref argv) {
+  PY_CHECK_ARGC(1);
+  Vector2 *self = py_touserdata(py_arg(0));
+  py_newfloat(py_retval(), sqrtf(self->x * self->x + self->y * self->y));
+  return true;
+}
+
+int main() {
+  py_initialize();
+
+  py_GlobalRef test = py_newmodule("test");
+  py_Type type = py_newtype("Vector2", tp_object, test, NULL);
+
+  py_bindmethod(type, "__new__", Vector2__new__);
+  py_bindmethod(type, "__init__", Vector2__init__);
+  py_bindproperty(type, "x", Vector2_get_x, Vector2_set_x);
+  py_bindproperty(type, "y", Vector2_get_y, Vector2_set_y);
+  py_bindmethod(type, "length", Vector2_length);
+
+  if (!py_exec(INPUT, "main.py", EXEC_MODE, NULL)) {
+    py_printexc();
+    goto finalize;
+  }
+
+finalize:
+  py_finalize();
+  return 0;
 }
